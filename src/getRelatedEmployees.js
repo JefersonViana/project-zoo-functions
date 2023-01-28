c4ecfaf4e34a1c078289c8a86e0d8eaa6d51affd
jefersonviana <jefersonv28@gmail.com>
2023-01-27 22:49:44 -0300

    Requisito 4 concluído

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
-const isManager = (id) => {
-  // seu código aqui
+const isManager = (id) => data.employees.some((objeto) => objeto
+  .managers[0] === id || objeto.managers[1] === id);
+
+const functionAux = (managerId) => {
+  const arrayColaboradores = [];
+  data.employees.forEach((objeto) => {
+    objeto.managers.forEach((elemento) => {
+      if (elemento === managerId) {
+        arrayColaboradores.push(`${objeto.firstName} ${objeto.lastName}`);
+      }
+    });
+  });
+  return arrayColaboradores;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    return functionAux(managerId);
+  }
 };
-
 module.exports = { isManager, getRelatedEmployees };
