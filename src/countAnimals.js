30982c6bf8aef89a582a501278a7889191d2fed8
jefersonviana <jefersonv28@gmail.com>
2023-01-28 10:12:43 -0300

    Requisito 5 concluído

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
+const functionAux = (chave, value) => {
+  const arrayDoObjetoFiltrado = data.species.filter((fullObjeto) => fullObjeto.name === chave);
+  const quantAnimals = arrayDoObjetoFiltrado[0].residents.filter((animal) => animal.sex === value);
+  return quantAnimals.length;
+};
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  const allSpecies = {};
+  if (!animal) {
+    data.species.forEach((objeto) => {
+      allSpecies[objeto.name] = objeto.residents.length;
+    });
+    return allSpecies;
+  }
+  let retornoEmQuantidade;
+  const key = Object.keys(animal);
+  if (key.length > 1) {
+    retornoEmQuantidade = functionAux(animal[key[0]], animal[key[1]]);
+  } else {
+    const specie = animal.species;
+    const arrayDoObjetoFiltrado = data.species
+      .filter((fullObjeto) => fullObjeto.name === specie);
+    retornoEmQuantidade = arrayDoObjetoFiltrado[0].residents.length;
+  }
+  return retornoEmQuantidade;
 };
 
 module.exports = countAnimals;
