fca040ca079fb84dc4d8d47a0803a4cec414ed6a
jefersonviana <jefersonv28@gmail.com>
2023-01-30 19:25:04 -0300

    Requisito 8 concluído

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter(({ age }) => age < 18);
+  const adult = entrants.filter(({ age }) => age >= 18 && age < 50);
+  const senior = entrants.filter(({ age }) => age >= 50);
+  const objVisitor = {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
+  return objVisitor;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const objectEntrants = countEntrants(entrants);
+  const { child: quantChild, adult: quantAdult, senior: quantSenior } = objectEntrants;
+  const { child, adult, senior } = prices;
+  return (child * quantChild) + (adult * quantAdult) + (senior * quantSenior);
 };
 
 module.exports = { calculateEntry, countEntrants };
