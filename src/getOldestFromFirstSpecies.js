dbba38ca6337a6ad0c3c9ed12036a6e740638614
jefersonviana <jefersonv28@gmail.com>
2023-01-31 20:35:45 -0300

    Requisito 10 concluído

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const idAnimals = employees.find(({ id: employeesId }) => employeesId === id).responsibleFor[0];
+  const arrayAnimals = species.find(({ id: specieId }) => specieId === idAnimals).residents;
+  let animalOlde = 0;
+  const objectReturn = {};
+  arrayAnimals.forEach((object) => {
+    const { age, sex, name } = object;
+    if (age > animalOlde) {
+      animalOlde = age;
+      objectReturn.name = name;
+      objectReturn.sex = sex;
+      objectReturn.age = age;
+    }
+  });
+  return [objectReturn.name, objectReturn.sex, objectReturn.age];
 };
 
 module.exports = getOldestFromFirstSpecies;
