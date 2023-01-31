2fcab7c919009c608afedd92788abbdc91c89c73
jefersonviana <jefersonv28@gmail.com>
2023-01-31 16:15:56 -0300

    Requisito 9 concluído

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,36 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
+
+const animals = (param) => {
+  const arrayAnimals = ['lions', 'tigers', 'bears', 'penguins', 'otters', 'frogs',
+    'snakes', 'elephants', 'giraffes'];
+  return arrayAnimals.some((index) => index === param);
+};
+
+const dayWeek = (param) => {
+  const arrayDayWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+    'Friday', 'Saturday', 'Sunday'];
+  return arrayDayWeek.some((index) => index === param);
+};
 
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  const visitacao = {};
+  const arrayTheDayWeek = Object.keys(hours);
+  arrayTheDayWeek.forEach((index) => {
+    const teste = species.filter(({ availability }) => availability
+      .some((elemento) => elemento === index)).map((object) => object.name);
+    visitacao[index] = {
+      officeHour: `Open from ${hours[index].open}am until ${hours[index].close}pm`,
+      exhibition: teste,
+    };
+  });
+  visitacao.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  if (animals(scheduleTarget) === true) {
+    return species.find(({ name }) => name === scheduleTarget).availability;
+  }
+  if (dayWeek(scheduleTarget) === true) {
+    return { [scheduleTarget]: visitacao[scheduleTarget] };
+  }
+  return visitacao;
 };
 
 module.exports = getSchedule;
