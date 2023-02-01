031931e7c718a802c4ce3ba20f8629dcda5ea2e7
jefersonviana <jefersonv28@gmail.com>
2023-02-01 20:03:14 -0300

    Começando a fazer o Requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,44 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
+const emplyParam = () => {
+  const objeto = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+  species.forEach((object) => {
+    switch (true) {
+      case object.location === 'NE':
+        objeto[object.location].push(object.name);
+        break;
+      case object.location === 'NW':
+        objeto[object.location].push(object.name);
+        break;
+      case object.location === 'SE':
+        objeto[object.location].push(object.name);
+        break;
+      case object.location === 'SW':
+        objeto[object.location].push(object.name);
+        break;
+      default:
+        break;
+    }
+  })
+  return objeto;
 };
 
+const getAnimalMap = (options) => {
+  if (!options) {
+    return emplyParam();
+  }
+  const keys = Object.keys(options);
+  const validation = keys.includes('includeNames');
+  console.log(validation);
+  if (keys.length === 1 && keys[0] === 'sex' || validation === false) {
+    return emplyParam();
+  }
+  return 'aconteceu nenhum if por isso cheguei aqui!';
+};
+console.log(getAnimalMap({ includeNames: 'teste', sex: 'female', sorted: true }));
 module.exports = getAnimalMap;
