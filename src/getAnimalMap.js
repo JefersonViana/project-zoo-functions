df56f1e1f6c7997a3a7a42a886e7436a948b0d06
jefersonviana <jefersonv28@gmail.com>
2023-02-02 17:14:31 -0300

    Requisito 14 concluído

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,44 +1,122 @@
 const { species } = require('../data/zoo_data');
 
-const emplyParam = () => {
-  const objeto = {
-    NE: [],
-    NW: [],
-    SE: [],
-    SW: [],
-  };
-  species.forEach((object) => {
-    switch (true) {
-      case object.location === 'NE':
-        objeto[object.location].push(object.name);
-        break;
-      case object.location === 'NW':
-        objeto[object.location].push(object.name);
-        break;
-      case object.location === 'SE':
-        objeto[object.location].push(object.name);
-        break;
-      case object.location === 'SW':
-        objeto[object.location].push(object.name);
-        break;
-      default:
-        break;
+const paramEmply = () => {
+  const objArrays = species.reduce((acc, objAtual) => {
+    acc[objAtual.location] = [];
+    species.forEach((animal) => {
+      if (animal.location === objAtual.location) {
+        acc[objAtual.location].push(animal.name);
+      }
+    });
+    return acc;
+  }, {});
+  return objArrays;
+};
+
+const nameTrue = () => {
+  const objArrays = {};
+  species.forEach((objAtual) => {
+    objArrays[objAtual.location] = [];
+    species.forEach((elemento) => {
+      if (objAtual.location === elemento.location) {
+        objArrays[objAtual.location].push({
+          [elemento.name]: elemento.residents
+            .map((resident) => resident.name),
+        });
+      }
+    });
+  });
+  return objArrays;
+};
+
+const sortedTrue = () => {
+  const objArrays = {};
+  species.forEach((objAtual) => {
+    objArrays[objAtual.location] = [];
+    species.forEach((elemento) => {
+      if (objAtual.location === elemento.location) {
+        objArrays[objAtual.location].push({
+          [elemento.name]: elemento.residents
+            .map((resident) => resident.name).sort(),
+        });
+      }
+    });
+  });
+  return objArrays;
+};
+const sexAnimal = (elemento, sex) => {
+  const arrayAnimalsSex = [];
+  elemento.residents.forEach((animal) => {
+    if (animal.sex === sex) {
+      arrayAnimalsSex.push(animal.name);
     }
-  })
-  return objeto;
+  });
+  return arrayAnimalsSex;
+};
+const sexAnimalSorted = (elemento, sex) => {
+  const arrayAnimalsSex1 = [];
+  elemento.residents.forEach((animal) => {
+    if (animal.sex === sex) {
+      arrayAnimalsSex1.push(animal.name);
+    }
+  });
+  return arrayAnimalsSex1.sort();
+};
+const sexTrue = (param) => {
+  const objArrays = {};
+  species.forEach((objAtual) => {
+    objArrays[objAtual.location] = [];
+    species.forEach((elemento) => {
+      if (objAtual.location === elemento.location) {
+        objArrays[objAtual.location].push({ [elemento.name]: sexAnimal(elemento, param) });
+      }
+    });
+  });
+  return objArrays;
+};
+const sexTrueSorted = (param) => {
+  const objArrays = {};
+  species.forEach((objAtual) => {
+    objArrays[objAtual.location] = [];
+    species.forEach((elemento) => {
+      if (objAtual.location === elemento.location) {
+        objArrays[objAtual.location].push({ [elemento.name]: sexAnimalSorted(elemento, param) });
+      }
+    });
+  });
+  return objArrays;
+};
+
+const validation2 = (includeNames, sex, sorted) => {
+  if (includeNames === true && sorted === undefined && typeof sex === 'string') {
+    return sexTrue(sex);
+  }
+  return sexTrueSorted(sex);
+};
+
+const validation1 = (includeNames, sex, sorted) => {
+  if (includeNames === true && sex === undefined && sorted === true) {
+    return sortedTrue();
+  }
+  return validation2(includeNames, sex, sorted);
+};
+
+const validation = (includeNames, sex, sorted) => {
+  if (includeNames === true && sex === undefined && sorted === undefined) {
+    return nameTrue();
+  }
+  return validation1(includeNames, sex, sorted);
 };
 
 const getAnimalMap = (options) => {
   if (!options) {
-    return emplyParam();
+    return paramEmply();
   }
-  const keys = Object.keys(options);
-  const validation = keys.includes('includeNames');
-  console.log(validation);
-  if (keys.length === 1 && keys[0] === 'sex' || validation === false) {
-    return emplyParam();
+  const { includeNames, sex, sorted } = options;
+  if (includeNames === undefined) {
+    return paramEmply();
   }
-  return 'aconteceu nenhum if por isso cheguei aqui!';
+  return validation(includeNames, sex, sorted);
 };
-console.log(getAnimalMap({ includeNames: 'teste', sex: 'female', sorted: true }));
+
 module.exports = getAnimalMap;
